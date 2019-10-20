0d0c130bd37f690a449fd7b0eca0c45738dec2ab
MichaelWiss <michael.wiss@gmail.com>
2019-10-20 10:49:09 -0500

    chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,9 @@
 const validator = require('validator')
 const getNotes = require('./notes.js')
+const chalk = require('chalk')
 
 
-
+const success = chalk.green;
 const msg = getNotes()
 
 
@@ -11,4 +12,5 @@ const msg = getNotes()
 console.log(msg)
 
 console.log(validator.isURL('http://michaelrwiss.com/'))
+console.log(success('Success!'));
 
