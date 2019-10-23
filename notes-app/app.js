708947f748447f9a3a8ca23208c72762458557eb
MichaelWiss <michael.wiss@gmail.com>
2019-10-23 17:05:05 -0500

    ifelse

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,15 +2,26 @@ const validator = require('validator')
 const getNotes = require('./notes.js')
 const chalk = require('chalk')
 
+const command = process.argv[2]
+
 
 const success = chalk.green.bgYellowBright;
 const msg = getNotes()
 
-
+console.log(process.argv)
 
 
 console.log(msg)
 
 console.log(validator.isURL('http://michaelrwiss.com/'))
 console.log(success('Success!'));
+console.log(success('Good Day!'));
+
+if (command === 'add') {
+	console.log('Adding note!')
+} else if (command === 'remove') {
+	console.log('Removing note')
+}
+
+
 
