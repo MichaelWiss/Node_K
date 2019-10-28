6e496da2bdddbc92a72e978b25464944e9bf18f6
MichaelWiss <michael.wiss@gmail.com>
2019-10-28 10:02:47 -0500

    using argv

--- notes-app/app.js
+++ notes-app/app.js
@@ -10,8 +10,15 @@ yargs.version('1.1.0')
 yargs.command({
      command: 'add',
      describe: 'Add a new note',
-     handler: function () {
-     	console.log('Adding a new note!')
+     builder: {
+     	title: {
+     		describe: 'Note title',
+     		demandOption: true,
+     		type: 'string'
+     	}
+     },
+      handler: function (argv) {
+     	console.log('Title ' + argv.title)
      }
 })
 
@@ -41,7 +48,7 @@ yargs.command({
 })
 
 
-console.log(yargs.argv)
+yargs.parse()
 
 
 
